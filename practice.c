f53c75568fe495f7b8dd29970c4662368afc994d
Dogukan Atar <dogukanatar@gmail.com>
2022-05-30 22:39:17 +0900

    commit 2

--- practice.c
+++ practice.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
 
-
-// variable declaration (only declaration)
-extern int a, b;
-extern int c;
-extern float f;
+//using define as preprocessor
+#define LENGTH 10
+#define WIDTH 5
+#define NEWLINE '\n'
 
 int main(){
+    int area;    
 
-    // definition of declated variables at the top
-    int a, b;
-    int c;
-    float f;
-
-    // actual initialization in the main function
-    a = 10;
-    b = 20;
-
-    c = a + b;
-    printf("Value of c: %d\n", c);
-    
-    f = 70.0/3.0;
-    printf("Value of f: %f\n", f);
+    area = LENGTH * WIDTH;
+    printf("the value of area: %d\n", area);
+    printf("%c", NEWLINE);
 
     return 0;
 
 }
+
+

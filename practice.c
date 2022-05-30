5ca3550dc72daa79b6da5c554620fca6aea49c00
Dogukan Atar <dogukanatar@gmail.com>
2022-05-30 16:50:01 +0900

    commit 1

--- practice.c
+++ practice.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+
+// variable declaration (only declaration)
+extern int a, b;
+extern int c;
+extern float f;
+
 int main(){
-    printf("Hello world");
+
+    // definition of declated variables at the top
+    int a, b;
+    int c;
+    float f;
+
+    // actual initialization in the main function
+    a = 10;
+    b = 20;
+
+    c = a + b;
+    printf("Value of c: %d\n", c);
+    
+    f = 70.0/3.0;
+    printf("Value of f: %f\n", f);
 
     return 0;
 

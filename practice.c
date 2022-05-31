6f1fc341c9f141890efd6692cf0a9a510c966513
Dogukan Atar <dogukanatar@gmail.com>
2022-05-31 14:28:32 +0900

    commit 4
    C storage classes

--- practice.c
+++ practice.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
+
+// function declaration
+void func(void);
+
+// static variable declaration and definition
+// global variable
+static int count = 5;
+
+// main function declaration
 int main(){
-
-    // Let's try const this time
-    const int LENGTH = 10;
-    const int WIDTH = 5;
-    const char NEWLINE = 'n';
-    int area;
-
-    area = LENGTH * WIDTH;
-    printf("the value of area: %d\n", area);
-    printf("%c", NEWLINE);
-
+    // while loop  
+    while (count--)
+    {
+        func();
+    }
     return 0;
 
 }
 
-
+void func(void){
+    // staic local variable
+    static int i = 5;
+    i++;
+    printf("i is %d and count is %d\n", i, count);
+}
